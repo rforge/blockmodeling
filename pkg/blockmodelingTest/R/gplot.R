6235cbf5aa32e4babb82768881498ea87c2164b1
aleszib <aleszib@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2018-04-11 07:30:29 +0000

    Making sna suggested package in blockmodelingTest
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/blockmodeling@100 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/blockmodelingTest/R/gplot.R
+++ pkg/blockmodelingTest/R/gplot.R
@@ -1,18 +1,22 @@
 "gplot1" <-function(M,diag=TRUE,displaylabels=TRUE,boxed.labels=FALSE,loop.cex=4,edge.lwd=1,edge.col="default",rel.thresh=0.05,...){
-  M[M<(max(M)*rel.thresh)]<-0  
-  if(edge.col[1]=="default") edge.col<-gray(1-M/max(M))
-  edge.col<-edge.col[edge.col!=gray(1)]
-  requireNamespace("sna",pos=max(grep(pattern="block",search()))+1)
-  gplot(dat=M,diag=diag,displaylabels=displaylabels,boxed.labels=boxed.labels,loop.cex=loop.cex,edge.lwd=edge.lwd,edge.col=edge.col,...)
+  if(!requireNamespace("sna", quietly = TRUE)){
+    M[M<(max(M)*rel.thresh)]<-0  
+    if(edge.col[1]=="default") edge.col<-gray(1-M/max(M))
+    edge.col<-edge.col[edge.col!=gray(1)]
+    sna::gplot(dat=M,diag=diag,displaylabels=displaylabels,boxed.labels=boxed.labels,loop.cex=loop.cex,edge.lwd=edge.lwd,edge.col=edge.col,...)
+  } else stop("Package \"sna\" is needed for this function to work. Please install it.",
+                   call. = FALSE)
 }
 
 
 "gplot2" <-
 function(M,uselen=TRUE,usecurve=TRUE,edge.len=0.001,diag=TRUE,displaylabels=TRUE,boxed.labels=FALSE,loop.cex=4,arrowhead.cex=2.5,edge.lwd=1,edge.col="default",rel.thresh=0.05,...){
-  M[M<(max(M)*rel.thresh)]<-0
-  if(edge.col[1]=="default") edge.col<-gray(1-M/max(M))
-  edge.col<-edge.col[edge.col!=gray(1)]
-  requireNamespace("sna",pos=max(grep(pattern="block",search()))+1)
-  gplot(dat=M,uselen=uselen,usecurve=usecurve,edge.len=edge.len,diag=diag,displaylabels=displaylabels,boxed.labels=boxed.labels,loop.cex=loop.cex,arrowhead.cex=arrowhead.cex,edge.lwd=edge.lwd,edge.col=edge.col,...)
+  if(!requireNamespace("sna", quietly = TRUE)){
+    M[M<(max(M)*rel.thresh)]<-0
+    if(edge.col[1]=="default") edge.col<-gray(1-M/max(M))
+    edge.col<-edge.col[edge.col!=gray(1)]
+    sna::gplot(dat=M,uselen=uselen,usecurve=usecurve,edge.len=edge.len,diag=diag,displaylabels=displaylabels,boxed.labels=boxed.labels,loop.cex=loop.cex,arrowhead.cex=arrowhead.cex,edge.lwd=edge.lwd,edge.col=edge.col,...)
+  } else stop("Package \"sna\" is needed for this function to work. Please install it.",
+              call. = FALSE)
 }
 
