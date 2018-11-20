8ae38e1644799d3abf1ddbfe503f8fd65ff10560
mihamatj <mihamatj@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2018-11-20 17:25:44 +0000

    Updating help files (clu.rd, critfunc.rd, plot.mat.rd) and R function IM.r
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/blockmodeling@162 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/blockmodeling/R/IM.R
+++ pkg/blockmodeling/R/IM.R
@@ -1,7 +1,18 @@
 "IM" <-
-function(res,which=1,...){
+function(res,which=1, drop=TRUE, ...){
   if(class(res)=="opt.more.par"){
-    return(res$best[[which]]$IM)
-  } else return(res$IM)
+    IM<-res$best[[which]]$IM
+  } else IM<-res$IM
+  if(drop)IM<-drop(IM)
+  return(IM)
+}
+
+"EM" <-
+function(res,which=1, drop=TRUE,...){
+    if(class(res)=="opt.more.par"){
+      EM<-res$best[[which]]$EM
+    } else EM<-res$EM
+    if(drop)EM<-drop(EM)
+    return(EM)
 }
 
