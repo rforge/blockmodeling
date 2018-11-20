947f1c83ffea5a6f239bf8fcaea8dcfe63d79c2c
mihamatj <mihamatj@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2018-11-20 20:17:54 +0000

    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/blockmodeling@163 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/blockmodeling/R/IM.R
+++ pkg/blockmodeling/R/IM.R
@@ -1,18 +1,7 @@
 "IM" <-
-function(res,which=1, drop=TRUE, ...){
+function(res,which=1,...){
   if(class(res)=="opt.more.par"){
-    IM<-res$best[[which]]$IM
-  } else IM<-res$IM
-  if(drop)IM<-drop(IM)
-  return(IM)
-}
-
-"EM" <-
-function(res,which=1, drop=TRUE,...){
-    if(class(res)=="opt.more.par"){
-      EM<-res$best[[which]]$EM
-    } else EM<-res$EM
-    if(drop)EM<-drop(EM)
-    return(EM)
+    return(res$best[[which]]$IM)
+  } else return(res$IM)
 }
 
