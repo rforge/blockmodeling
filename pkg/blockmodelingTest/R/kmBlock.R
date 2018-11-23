2e3f4d3287598359357f9937f1ccf4bb8b5c82a3
aleszib <aleszib@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2018-11-23 09:30:20 +0000

    Added functions for k-means blockmodeling and helper functions to blockmodelingTest. These are highly EXPERIMENTAL. Updated NAMESPACE, DESCRIPTION and CHANGES files accordingly.
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/blockmodeling@170 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

---
+++ pkg/blockmodelingTest/R/kmBlock.R
@@ -0,0 +1,400 @@
+#' Function that performs k-means like one-mode blockmodeling. If \code{clu} is a list, the method for linked/multilevel networks is applied
+#'
+#' @param M A matrix representing a network (multirelational networks are currently not supported)
+#' @param clu A partition. Each unique value represents one cluster. If the nework is one-mode, than this should be a vector, else a list of vectors, one for each mode. Similarly, if units are comprised of several sets, clu should be the list containing one vector for each set.
+#' @param eps When the sum of squared differences in block means is less than \code{eps}, the algorithm terminates. Defaults to 1
+#' @param each Should the block means be computed after each unit is reassigned. Defaults to \code{FALSE}, as otherwise this is too slow.
+#' @param weights The weights for each cell in the matrix. A matrix with the same dimmensions as \code{M}. 
+#' @param limits The matrix with dimentsions "number of clusters"x"number of clusters" where each element is a function. The functions are applied to the computed block means and modify them. This can be used to "pre-specify" them.  
+#' @return A list similar to optParC
+kmBlock<-function(M, 
+                  clu, 
+                  eps=0, 
+                  each=FALSE, 
+                  weights=NULL, 
+#                  eachProb=FALSE, 
+#                  nMode=NULL, 
+                  limits=NULL){
+  n<-dim(M)[1]
+  MnoDiag<-M
+  diag(MnoDiag)<-NA
+  
+  
+  if(is.null(weights)){
+    weights<-M
+    weights[,]<-1
+  } else if(any(dim(weights)!=dim(M))) stop("Weights have wrong dim!")
+  w<-weights
+  
+#  if(is.null(nMode)) nMode<-ifelse(is.list(clu),length(clu),1)
+  nMode<-ifelse(is.list(clu),length(clu),1)
+  
+  if(nMode>1){
+    tmN<-sapply(clu,length)
+    clu<-lapply(clu,function(x)as.integer(factor(x)))
+    tmNclu<-sapply(clu,max)
+    for(iMode in 2:nMode){
+      clu[[iMode ]]<-clu[[iMode ]]+sum(tmNclu[1:(iMode -1)])
+    }
+    
+    clu<-unlist(clu)
+    
+    Means<-diag(fun.by.blocks(M,clu = rep(1:nMode,times=tmN), fun="mean",ignore.diag = TRUE))
+  } else{
+    clu<-as.integer(factor(clu))
+    tmNclu<-max(clu)
+    tmN<-length(clu)
+    Means<-mean(MnoDiag, na.rm=TRUE)
+  }
+  Mode<-rep(1:nMode,times=tmNclu)
+  
+  ssMin<-rep(Inf, n)
+  
+  clu<-as.integer(factor(clu))
+  k<-max(clu)
+  IM<-fun.by.blocks(M,clu=clu, FUN="mean",ignore.diag = TRUE)
+  IMna<-which(is.na(diag(IM)))
+  if(length(IMna)>0){
+    diag(IM)[IMna]<-Means[Mode[IMna]]
+  }
+  
+  tclu<-table(clu)
+  oldIM<-IM
+  oldIM[,]<-1
+  if(!is.null(limits)){
+    for(i in 1:k)for(j in 1:k){
+      IM[i,j] <- limits[[i,j]](IM[i,j])
+    }
+  }
+  
+  #err<-Inf
+  while(sum((oldIM-IM)^2)>eps){
+    iMode<-1
+    tresh<-tmN[1]
+    allowedClus<- 1:tmNclu[1]
+    oldIM<-IM
+    oldClu<-clu
+    for(i in 1:n){
+      x<-c(M[i,-i],M[-i,i])
+      iw<-c(w[i,-i],w[-i,i])
+      ssi<-rep(Inf, k)
+      if(i>tresh){
+        iMode<-iMode+1
+        tresh<-tresh+tmN[iMode]
+        allowedClus<- (cumsum(tmNclu)[iMode-1]+1):cumsum(tmNclu)[iMode]
+      }    
+      for(j in allowedClus){
+        p<-c(IM[j,clu[-i]],IM[clu[-i],j])
+        ssi[j]<-sum(iw*(x-p)^2)
+      }
+      clu[i]<-which.min(ssi)
+      ssMin[i]<-min(ssi)
+        
+      if(each){
+        tclu<-table(clu)
+        if(length(tclu)<k){
+          clu<-oldClu
+          IM<-oldIM
+          eps<-Inf
+          break
+        }        
+        IM<-fun.by.blocks(M,clu=clu, FUN="mean",ignore.diag = TRUE)
+        IMna<-which(is.na(diag(IM)))
+        if(length(IMna)>0){
+          diag(IM)[IMna]<-Means[Mode[IMna]]
+        }
+        
+        if(!is.null(limits)){
+          for(i in 1:k)for(j in 1:k){
+            IM[i,j] <- limits[[i,j]](IM[i,j])
+          }
+        }
+      }
+    }
+    tclu<-table(clu)
+    
+    ### old version
+    # if(length(tclu)<k){
+    #   clu<-oldClu
+    #   IM<-oldIM
+    #   oldIM[,]<-1
+    #   if(each|!eachProb) break
+    #   each<-TRUE
+    #   tclu<-table(clu)
+    #   ptclu<-tclu/sum(tclu)
+    #   lptclu<-log(ptclu)
+    #   next
+    # }
+    
+    while(length(tclu)<k){
+      missingClus<-(1:k)[!(as.character(1:k) %in% names(table(clu)))]
+      iClu<-sample(missingClus,size = 1)
+      iMode<-Mode[iClu]
+      allowedClus<- (c(0,cumsum(tmNclu))[iMode]+1):cumsum(tmNclu)[iMode]
+      ids<-which(clu%in%allowedClus)
+      selectId<-ids[which.max(ssMin[ids])]
+      clu[selectId]<-iClu
+      ssMin[selectId]<-min(ssMin)
+      tclu<-table(clu)
+    }
+
+    
+    IM<-fun.by.blocks(M,clu=clu, FUN="mean",ignore.diag = TRUE)
+    IMna<-which(is.na(diag(IM)))
+    if(length(IMna)>0){
+      diag(IM)[IMna]<-Means[Mode[IMna]]
+    }
+    
+    if(!is.null(limits)){
+      for(i in 1:k)for(j in 1:k){
+        IM[i,j] <- limits[[i,j]](IM[i,j])
+      }
+    }
+#    oldErr<-err
+#    err<-sum(w*(MnoDiag-IM[clu,clu])^2,na.rm=TRUE)
+#    if(oldErr<=err) print(c(old=oldErr, new=err))
+  }
+  err<-sum(w*(MnoDiag-IM[clu,clu])^2,na.rm=TRUE)
+  
+  res<-list(M=M, clu=clu, IM=IM, err=err, best=list(list(M=M, clu=clu, IM=IM)))
+  class(res)<-"opt.par"
+  return(res)
+}
+
+
+#' A function for optimizing multiple random partitions using k-means like blockmodeling. Similar to optRandomParC, but calling kmBlock for optimizing individual partitions.
+#'
+#' @inheritParams optRandomParC
+#' @return A list similar to optRandomParC
+
+kmBlockORP<-function(M, #a square matrix
+                        k,#number of clusters/groups
+                        rep,#number of repetitions/different starting partitions to check
+                        save.initial.param=TRUE,  #save the initial parametrs of this call
+                        save.initial.param.opt=FALSE,  #save the initial parametrs for calls to optParC or optParMultiC
+                        deleteMs=TRUE, #delete networks/matrices from results of optParC or optParMultiC to save space
+                        max.iden=10, #the maximum number of results that should be saved (in case there are more than max.iden results with minimal error, only the first max.iden will be saved)
+                        return.all=FALSE,#if 'FALSE', solution for only the best (one or more) partition/s is/are returned
+                        return.err=TRUE,#if 'FALSE', only the resoults of crit.fun are returned (a list of all (best) soulutions including errors), else the resoult is list
+                        seed=NULL,#the seed for random generation of partitions
+                        RandomSeed=NULL, # the state of .Random.seed (e.g. as saved previously). Should not be "typed" by the user
+                        parGenFun = genRandomPar, #The function that will generate random partitions. It should accept argumetns: k (number of partitions by modes, n (number of units by modes), seed (seed value for random generation of partition), addParam (a list of additional parametres)
+                        mingr=NULL, #minimal alowed group size (defaults to c(minUnitsRowCluster,minUnitsColCluster) if set, else to 1) - only used for parGenFun function 
+                        maxgr=NULL, #maximal alowed group size (default to c(maxUnitsRowCluster,maxUnitsColCluster) if set, else to Inf) - only used for parGenFun function 
+                        addParam=list(  #list of additional parameters for gerenrating partitions. Here they are specified for dthe default function "genRandomPar"
+                          genPajekPar = TRUE,     #Should the partitions be generated as in Pajek (the other options is completly random)
+                          probGenMech = NULL),    #Here the probabilities for different mechanizems for specifying the partitions are set. If not set this is determined based on the previous parameter.
+                        maxTriesToFindNewPar=rep*10,    #The maximum number of partition try when trying to find a new partition to optimize that was not yet checked before 
+                        skip.par = NULL, #partitions to be skiped
+                        printRep= ifelse(rep<=10,1,round(rep/10)), #should some information about each optimization be printed
+                        n=NULL, #the number of units by "modes". It is used only for generating random partitions. It has to be set only if there are more than two modes or if there are two modes, but the matrix representing the network is onemode (both modes are in rows and columns)
+                        nCores=1, #number of cores to be used 0 -means all available cores, can also be a cluster object,
+                        useParLapply=FALSE, #should ply be used instead of foreach
+                        cl = NULL, #the cluster to use (if formed beforehand)
+                        stopcl = FALSE, # should the cluster be stoped
+                        ... #paramters to kmBlock
+){
+  dots<-list(...)
+
+  
+  if(save.initial.param)initial.param<-c(tryCatch(lapply(as.list(sys.frame(sys.nframe())),eval),error=function(...)return("error")),dots=list(...))#saves the inital parameters
+  
+  if(is.null(mingr)){
+    if(is.null(dots$minUnitsRowCluster)){
+      mingr<-1
+    } else {
+      mingr<-c(dots$minUnitsRowCluster,dots$minUnitsColCluster)
+    }
+  }
+  
+  if(is.null(maxgr)){
+    if(is.null(dots$maxUnitsRowCluster)){
+      maxgr<-Inf
+    } else {
+      maxgr<-c(dots$maxUnitsRowCluster,dots$maxUnitsColCluster)
+    }
+  }
+  
+  nmode<-length(k)
+  
+  res<-list(NULL)
+  err<-NULL
+  dots<-list(...)
+  
+  if(save.initial.param)initial.param<-c(tryCatch(lapply(as.list(sys.frame(sys.nframe())),eval),error=function(...)return("error")),dots=list(...))#saves the inital parameters
+  
+  
+  if(is.null(n)) if(nmode==1){
+    n<-dim(M)[1]
+  } else if(nmode==2){
+    n<-dim(M)[1:2]
+  } else warning("Number of nodes by modes can not be determined. Parameter 'n' must be supplied!!!")
+  
+  if(!is.null(RandomSeed)){
+    .Random.seed <-  RandomSeed
+  } else if(!is.null(seed))set.seed(seed)
+  
+  
+  on.exit({
+    res1 <- res[which(err==min(err, na.rm = TRUE))]
+    best<-NULL
+    best.clu<-NULL
+    for(i in 1:length(res1)){
+      for(j in 1:length(res1[[i]]$best)){
+        if(
+          ifelse(is.null(best.clu),
+                 TRUE,
+                 if(nmode==1){
+                   !any(sapply(best.clu,rand2,clu2=res1[[i]]$clu)==1)
+                 } else {
+                   !any(sapply(best.clu,function(x,clu2)rand2(unlist(x),clu2),clu2=unlist(res1[[i]]$clu))==1)
+                  }
+          )
+        ){
+          best<-c(best,res1[i])
+          best.clu<-c(best.clu,list(res1[[i]]$clu))
+        }
+        
+        if(length(best)>=max.iden) {
+          warning("Only the first ",max.iden," solutions out of ",length(na.omit(err))," solutions with minimal sum of square deviations will be saved.\n")
+          break
+        }
+        
+      }
+    }
+    
+    names(best)<-paste("best",1:length(best),sep="")
+    
+    if(any(na.omit(err)==-Inf) || ss(na.omit(err))!=0 || length(na.omit(err))==1){
+      cat("\n\nOptimization of all partitions completed\n")
+      cat(length(best),"solution(s) with minimal sum of square deviations =", min(err,na.rm=TRUE), "found.","\n")
+    }else {
+      cat("\n\nOptimization of all partitions completed\n")
+      cat("All",length(na.omit(err)),"solutions have sum of square deviations",err[1],"\n")
+    }
+    
+    call<-list(call=match.call())
+    best<-list(best=best)
+    checked.par<-list(checked.par=skip.par)
+    if(return.all) res<-list(res=res) else res<-NULL
+    if(return.err) err<-list(err=err) else err<-NULL
+    if(!exists("initial.param")){
+      initial.param<-NULL
+    } else initial.param=list(initial.param)
+    
+    res<-c(list(M=M),res,best,err,checked.par,call,initial.param=initial.param, list(Random.seed=.Random.seed, cl=cl))
+    class(res)<-"opt.more.par"
+    return(res)
+  })
+  
+  
+  
+  if(nCores==1||!require(parallel)){
+    if(nCores!=1) {
+      oldWarn<-options("warn")
+      options(warn=1)
+      warning("Only single core is used as package 'parallel' is not available")
+      options(warn=oldWarn)
+    }
+    for(i in 1:rep){
+      if(printRep & (i%%printRep==0)) cat("\n\nStarting optimization of the partiton",i,"of",rep,"partitions.\n")
+      find.unique.par<-TRUE
+      ununiqueParTested=0
+      while(find.unique.par){
+        temppar<-parGenFun(n=n,k=k,mingr=mingr,maxgr=maxgr,addParam=addParam)
+        
+        find.unique.par<-
+          ifelse(is.null(skip.par),
+                 FALSE,
+                 if(nmode==1) {
+                   any(sapply(skip.par,rand2,clu2=temppar)==1)
+                 } else any(sapply(skip.par,function(x,clu2)rand2(unlist(x),clu2),clu2=unlist(temppar))==1)
+          )
+        ununiqueParTested=ununiqueParTested+1
+        endFun<-ununiqueParTested>=maxTriesToFindNewPar
+        if(endFun) {
+          break
+        } else if(ununiqueParTested%%10==0) cat(ununiqueParTested,"partitions tested for unique partition\n")
+      }
+      
+      if(endFun) break
+      
+      skip.par<-c(skip.par,list(temppar))
+      
+      if(printRep==1) cat("Starting partition:",blockmodeling:::unlistPar(temppar),"\n")
+      res[[i]]<-kmBlock(M=M, clu=temppar,  ...)
+      if(deleteMs){
+        res[[i]]$M<-NULL
+      }
+      res[[i]]$best<-NULL
+
+      err[i]<-res[[i]]$err
+      if(printRep==1) cat("Final sum of square deviations:",err[i],"\n")
+      if(printRep==1) cat("Final partition:   ",blockmodeling:::unlistPar(res[[i]]$clu),"\n")
+    }
+  } else {
+    oneRep<-function(i,M,n,k,mingr,maxgr,addParam,rep,...){
+      temppar<-parGenFun(n=n,k=k,mingr=mingr,maxgr=maxgr,addParam=addParam)
+      
+      #skip.par<-c(skip.par,list(temppar))
+      
+      tres <- try(kmBlock(M=M, clu=temppar,  ...))
+      if(class(tres)=="try-error"){
+        tres<-list("try-error"=tres, err=Inf, startPart=temppar)
+      }
+      if(deleteMs){
+        tres$M<-NULL
+      }
+      tres$best<-NULL
+      return(list(tres))
+    }
+    
+    if(!require(doParallel)|!require(doRNG)) useParLapply<-TRUE
+    
+    if(nCores==0){
+      nCores<-detectCores()-1                    
+    }
+    
+    if(useParLapply) {
+      if(is.null(cl)) cl<-makeCluster(nCores)
+      clusterSetRNGStream(cl)
+      nC<-nCores
+      #clusterExport(cl, varlist = c("kmBlock","kmBlockORP"))
+      clusterExport(cl, varlist = "kmBlock")
+      clusterEvalQ(cl, expr=library(blockmodeling))
+      res<-parLapplyLB(cl = cl,1:rep, fun = oneRep, M=M,n=n,k=k,mingr=mingr,maxgr=maxgr,addParam=addParam,rep=rep,...)
+      if(stopcl) stopCluster(cl)
+      res<-lapply(res,function(x)x[[1]])
+    } else {
+      library(doParallel)
+      library(doRNG)
+      if(is.null(cl)) cl<-makeCluster(nCores)
+      if(!getDoParRegistered()){
+        registerDoParallel(cl)
+      }
+      nC<-getDoParWorkers()
+      res<-foreach(i=1:rep,.combine=c, .packages='blockmodeling', .export = c("kmBlock")) %dorng% oneRep(i=i,M=M,n=n,k=k,mingr=mingr,maxgr=maxgr,addParam=addParam,rep=rep,...)
+    }
+    err<-sapply(res,function(x)x$err)    
+  }
+}
+
+
+
+#' Internal testing function (compute weighted error for sum of squares ignoring the diagonal)
+ssModel<-function(M, clu, w=NULL){
+  n<-dim(M)[1]
+  if(is.null(w))w<-matrix(1, ncol=n, nrow=n)
+  clu<-as.numeric(factor(clu))
+  k<-max(clu)
+  IM<-fun.by.blocks(M,clu=clu, FUN="mean",ignore.diag = TRUE)
+  oldIM<-IM
+  oldIM[,]<-1
+  MnoDiag<-M
+  diag(MnoDiag)<-NA
+  #  ll<-sum(tclu*lptclu)+sum(MnoDiag*log(IM[clu,clu]),na.rm=TRUE)+sum((1-MnoDiag)*log(1-IM[clu,clu]),na.rm=TRUE)
+  #  print(ll)
+  err<-sum(w*(MnoDiag-IM[clu,clu])^2,na.rm=TRUE)
+  return(err)
+}
+
---
+++ pkg/blockmodelingTest/R/mlKmeasHelperFun.R
@@ -0,0 +1,250 @@
+#' A function that save an object given to x in a file named with the name of the object and an edning ".Rdata". A different filename can be aslo suplied, but then this is the same as save.
+#'
+#' @param x An object to be saved.
+#' @param fileName A filename into which the object should be saved. Usually omitted and constructed automatically.
+saveObj<-function(x,fileName=NULL){
+    objName <- deparse(substitute(x))
+    if (is.null(fileName) & (!is.null(objName))) 
+    objName<-deparse(substitute(x)) 
+    if(is.null(fileName)&(!is.null(objName))) fileName<-paste(objName,".Rdata",sep="")
+    if(!is.null(fileName)) save(list=objName, file = fileName) else stop("Suitable filename was not provided and could not be created based on object name")
+}
+
+
+#' A function that loads an object with the name used as the first arguemnt from a file named with the name of the object and an edning ".Rdata". A different filename can be aslo suplied, but then this is the same as load. Reverse of saveObj.
+#'
+#' @param x An object to be loaded (acctually unquoted filename without the ".Rdata" ending from which the object is to be loaded. The function does not check if the loaded object is of the same name.
+#' @param fileName A filename from which the object should be loaded. Usually omitted and constructed automatically.
+loadObj<-function(x,fileName=NULL){
+  objName <- deparse(substitute(x))
+  if(is.null(fileName)&(!is.null(objName))) fileName<-paste(objName,".Rdata",sep="")
+  load(file = fileName,.GlobalEnv)
+}
+
+#' For a vector x, it computes x[1]/x.
+#'
+#' @param x A numeric vector. Should not contain 0s.
+#' @return A vector computed as x[1]/x.
+relInv<-function(x)x[1]/x
+
+
+#' For a vector x, it computes x[1]/x. If result is not finite (e.g. if certain elements of x are 0), the result is 0.
+#'
+#' @param x A numeric vector. Can contain 0s.
+#' @return A vector computed as x[1]/x. If result is not finite (e.g. if certain elements of x are 0), the result is 0.
+relInv2<-function(x){
+  x<-x[1]/x
+  x[!is.finite(x)]<-0
+  x
+}
+
+#' Computes sum of squared deviations from the mean using only valid (non NA) values.
+#'
+#' @param x A vector (or something that can be coerced to a vector) of values (which can include NAs)
+#' @return Sum of squared deviations from the mean using only valid (non NA) values.
+ssNa<-function(x)ss(na.omit(as.vector(x)))
+
+
+#' Expands a square matrix by repeating each row/column the specified number of times
+#'
+#' @param mat A square matrix to be exapanded
+#' @param nn A vector of number of times each row/column must be repeated. Its length must match the number of rows/columns
+#' @return Sum of squared deviations from the mean using only valid (non NA) values.
+expandMat<-function(mat, nn){
+    v<-rep(1:length(nn),nn)
+  mat[v,v]
+}
+
+#' Performed first k-means approach folowed by a homogeneity sum of squared generalized blockmodeling approach using only complete blocks
+#'
+#' @inheritParams kmBlockORP
+#' @param orFun kmBlock currently requires M to be a matrix. If M is a 3 dimmensional array with the third dimmension repesenting relation, the function specified by orFun (must be a function object) is used to combine different relations
+#' @param blocks Allowed blocks or pre-specified block image for sum of squares blockmodeling approach. Anyting else but the default probably does not make sense. 
+#' @return Sum of squared deviations from the mean using only valid (non NA) values.
+kMeansAndSSMlevOR<-function(M, k, nn=ncol(M), weightMat=matrix(1), rep=1000, nCores=0, orFun=max, blocks="com", ...){
+  
+  if(length(dim(M))>2)if(length(dim(M))==3){
+    M<-apply(M, c(1,2),sum)
+  }else stop("M must have 2 or 3 dimmensions")
+  tmpW<-expandMat(weightMat,nn)
+  
+  posWeights<-expandMat(weightMat,k)
+  
+  kmRes<-kmBlockORP(M = M,k = k,n = nn,weights = tmpW, rep = rep, nCores = nCores, ...)
+  tclu<-splitCluRes(kmRes)
+  if(length(tclu)==1) tclu<-unlist(tclu)
+  ssRes<-optParC(M = M, clu=tclu, approach="hom",blocks=blocks,posWeights = posWeights,useMulti = TRUE)
+  ssRes$kmRes<-kmRes
+  return(ssRes)
+}
+
+
+
+#' Replaces NaN values by the speficied values (0 by default)
+#'
+#' @param x A vector or similar where the NaNs are to be replaced.
+#' @param rep A value that should replace the NaNs (0 by default).
+#' @return x with NaNs replaced by rep.
+nanRep<-function(x, rep=0){
+  x[is.nan(x)]<-rep
+  return(x)
+}
+
+
+
+#' Computes weights for parts of the multilevel network based on random errors using the SS approach with complete blocks only (compatible with k-means)
+#'
+#' @param mlNet A multilevel/linked network
+#' @param cluParts A partition spliting the units into different sets
+#' @param k A vecotor of number of clusters for each set of units in the network.
+#' @param mWeights The number of repetitions for computing random errors. Defaults to 1000
+#' @return Weights and "intermediate results":
+#'  \item{"errArr"}{A 3d array of errors (\code{mWeights} for each part of the network)}
+#'  \item{"errMatSum"}{\code{errArr} summed over all repretitions.}
+#'  \item{"weightsMat"}{A matrix of weights, one for each part. An inverse of \code{errMatSum} with NaNs replaced by zeros.}
+weightsMlSS<-function(mlNet,cluParts, k, mWeights=1000){
+  cluParts<-as.numeric(factor(cluParts))
+  nn<-table(cluParts)
+  mlOrNet<-apply(mlNet, c(1,2),sum)
+  parts<-fun.by.blocks(mlOrNet, clu = cluParts, ignore.diag = FALSE, FUN = ss)>0
+  errArr<-array(NA,dim=c(dim(parts),mWeights))
+  
+  for(i1 in 1:dim(parts)[1]){
+    for(i2 in 1:dim(parts)[2]){
+      if(is.finite(parts[i1,i2])&parts[i1,i2]){
+        net<-mlOrNet[cluParts==i1, cluParts==i2]
+        if(i1!=i2){
+          tnet<-matrix(0, nrow=(nn[i1] + nn[i2]), ncol=(nn[i1] + nn[i2]))
+          tnet[cluParts[cluParts %in% c(i1,i2)]==i1, cluParts[cluParts %in% c(i1,i2)]==i2]<-net
+          net<-tnet
+        }
+        for(i in 1:mWeights){
+          if(i1==i2){
+            tclu<-genRandomPar(k = k[i1],n = nn[i1],addParam=list(genPajekPar = FALSE))
+          } else {
+            tclu<-genRandomPar(k = k[c(i1,i2)],n = nn[c(i1,i2)],addParam=list(genPajekPar = FALSE))
+            tclu[[2]]<-tclu[[2]]+k[i1]
+            tclu<-unlist(tclu)
+          }
+          tCF<-critFunC(net, clu=tclu, approach="hom",homFun="ss", blocks="com")
+          
+          errArr[i1,i2,i]<-tCF$err
+        }
+      }
+      cat("i1 = ", i1, ", i2 = ", i2,"\n")
+    }
+  }
+  errMatSum<-apply(errArr,c(1,2),sum)
+  weightsMat<-relInv2(errMatSum)
+  res<-list(errArr=errArr, errMatSum=errMatSum, weightsMat=weightsMat)
+}
+
+
+
+
+
+#' Computes errors for parts of the multilevel network using the SS approach with complete blocks only (compatible with k-means)
+#'
+#' @param mlNet A multilevel/linked network
+#' @param cluParts A partition spliting the units into different sets.
+#' @param nn A vector of mumber of units in each set. Either this or \code{cluParts} must be suplied
+#' @param clu A partition for which the errors by parts should be comptued.
+#' @return \code{x} with NaNs replaced by rep.
+errByPartsMlSS<-function(mlNet,cluParts=NULL, nn=NULL, clu){
+  if(is.null(cluParts)){
+    if(is.null(nn)) stop("Either cluParts or nn must be supplied")
+    cluParts<-rep(1:length(nn),times=nn)
+  }
+  if(!is.list(clu)){
+    clu<-split(clu,cluParts)
+  }
+  cluParts<-as.numeric(factor(cluParts))
+  nn<-table(cluParts)
+  mlOrNet<-apply(mlNet, c(1,2),sum)
+  parts<-fun.by.blocks(mlOrNet, clu = cluParts, ignore.diag = FALSE, FUN = ss)>0
+  errMat<-parts
+  errMat[,]<-NA
+  
+  for(i1 in 1:dim(parts)[1]){
+    for(i2 in 1:dim(parts)[2]){
+      if(is.finite(parts[i1,i2])&parts[i1,i2]){
+        net<-mlOrNet[cluParts==i1, cluParts==i2]
+        if(i1!=i2){
+          tnet<-matrix(0, nrow=(nn[i1] + nn[i2]), ncol=(nn[i1] + nn[i2]))
+          tnet[cluParts[cluParts %in% c(i1,i2)]==i1, cluParts[cluParts %in% c(i1,i2)]==i2]<-net
+          net<-tnet
+        }
+          if(i1==i2){
+          tclu<-clu[[i1]]
+          tclu<-as.numeric(factor(tclu))
+        } else {
+          tclu<-clu[c(i1,i2)]
+          
+          tclu<-lapply(tclu,function(x)as.numeric(factor(x)))
+          tclu[[2]]<-tclu[[2]]+max(tclu[[1]])
+          tclu<-unlist(tclu)
+        }
+        tCF<-critFunC(net, clu=tclu, approach="hom",homFun="ss", blocks="com")
+        
+        errMat[i1,i2]<-tCF$err
+    }
+      #cat("i1 = ", i1, ", i2 = ", i2,"\n")
+    }
+  }
+  return(errMat)
+}
+
+
+#' Optimizes the partitions for parts of the multilevel network using the k-means approach folowed by SS approach with complete blocks only (compatible with k-means)
+#'
+#' @param mlNet A multilevel/linked network
+#' @param cluParts A partition spliting the units into different sets.
+#' @param nn A vector of mumber of units in each set. Either this or \code{cluParts} must be suplied
+#' @param k A vector of the number of clusters (one for each set of units).
+#' @param rep The number of random restarts to try.
+#' @return \code{x} with NaNs replaced by rep.
+optErrByPartsMlSS<-function(mlNet,cluParts=NULL, nn=NULL, k, rep=100, nCores=0){
+  if(is.null(cluParts)){
+    if(is.null(nn)) stop("Either cluParts or nn must be supplied")
+    cluParts<-rep(1:length(nn),times=nn)
+  }
+  clu<-cluParts
+  clu<-as.numeric(factor(clu))
+  nn<-table(clu)
+  mlOrNet<-apply(mlNet, c(1,2),sum)
+  parts<-fun.by.blocks(mlOrNet, clu = clu, ignore.diag = FALSE, FUN = ss)>0
+  errMat<-parts
+  errMat[,]<-NA
+  
+  for(i1 in 1:dim(parts)[1]){
+    for(i2 in 1:dim(parts)[2]){
+      if(is.finite(parts[i1,i2])&parts[i1,i2]){
+        net<-mlOrNet[clu==i1, clu==i2]
+      
+        if(i1!=i2){
+          tnet<-matrix(0, nrow=(nn[i1] + nn[i2]), ncol=(nn[i1] + nn[i2]))
+          tnet[clu[clu %in% c(i1,i2)]==i1, clu[clu %in% c(i1,i2)]==i2]<-net
+          net<-tnet
+          
+          tmpK<-k[c(i1,i2)]
+          
+          n = nn[c(i1,i2)]
+        } else {
+          tmpK<-k[i1]
+          n = nn[i1]
+        }
+        kmRes<-kmBlockORP(M = net,k = tmpK,n = n, rep = rep, nCores = nCores)
+        
+        if(i1==i2){
+          tclu<-clu(kmRes)  
+        } else tclu<-splitCluRes(kmRes)
+        
+        ssRes<-optParC(M = net, clu=tclu, approach="hom", blocks="com", useMulti = TRUE)
+        
+          errMat[i1,i2]<-ssRes$err
+        
+      }
+      cat("i1 = ", i1, ", i2 = ", i2,"\n")
+    }
+  }
+  return(errMat)}
