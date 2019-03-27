104910c13f2571f9b1a25bb39aace6c35bdb2540
aleszib <aleszib@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2019-03-27 09:34:04 +0000

    Updated tests.
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/blockmodeling@248 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/kMeans/Tests/simPaper.R
+++ pkg/kMeans/Tests/simPaper.R
@@ -32,10 +32,31 @@ attr(test60sHomCmoveAndChange,which = "totalTime")<-tmp
 test60sHomCmoveAndChange$nStarts
 
 
+tmp<-system.time(test60sHomCjustMove<-kmBlock::testORPtimeLim(M, k=k, blockFun = blockmodelingTest::optParC, timeLim = 5,approach="hom", blocks="com",justMove=TRUE))
+attr(test60sHomCjustMove,which = "totalTime")<-tmp
+test60sHomCjustMove$nStarts
 
-homCmoveAndChangeTime<-system.time(res<-blockmodelingTest::optRandomParC(M,k=k,rep=nRep,approach="hom", blocks="com"))
 
-homCjustMoveTime<-system.time(res<-blockmodelingTest::optRandomParC(M,k=k,rep=nRep,approach="hom", blocks="com",justChange=TRUE))
+
+tmp<-system.time(test60sHomCmoveAndChangeMulti<-kmBlock::testORPtimeLim(M, k=k, blockFun = blockmodelingTest::optParC, timeLim = 5,approach="hom", blocks="com", useMulti = TRUE))
+
+attr(test60sHomCmoveAndChangeMulti,which = "totalTime")<-tmp
+test60sHomCmoveAndChangeMulti$nStarts
+
+
+tmp<-system.time(test60sHomCjustMoveMulti<-kmBlock::testORPtimeLim(M, k=k, blockFun = blockmodelingTest::optParC, timeLim = 5,approach="hom", blocks="com",justMove=TRUE, useMulti = TRUE))
+attr(test60sHomCjustMoveMulti,which = "totalTime")<-tmp
+test60sHomCjustMoveMulti$nStarts
+
+
+
+seed<-sample(1000000,1)
+set.seed(seed)
+homCmoveAndChangeTime<-system.time(homCmoveAndChangeRes<-blockmodelingTest::optRandomParC(M,k=k,rep=nRep,approach="hom", blocks="com",useMulti=FALSE))
+set.seed(seed)
+homCjustMoveTime<-system.time(homCjustMoveRes<-blockmodelingTest::optRandomParC(M,k=k,rep=nRep,approach="hom", blocks="com",justChange=TRUE,useMulti=FALSE))
+homCmoveAndChangeRes$err
+homCjustMoveRes$err
 
 rbind(kmBlockORPCtime,kmBlockORPtime,homCmoveAndChangeTime,homCjustMoveTime)
 
