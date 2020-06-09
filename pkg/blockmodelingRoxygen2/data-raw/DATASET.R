f28357241ab6a75e5af577f76d31964ef4a4990d
mihamatj <mihamatj@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2020-06-09 07:08:22 +0000

    Raw baker data in the package
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/blockmodeling@293 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/blockmodelingRoxygen2/data-raw/DATASET.R
+++ pkg/blockmodelingRoxygen2/data-raw/DATASET.R
@@ -1,13 +1,3 @@
 ## code to prepare `DATASET` dataset goes here
 
 usethis::use_data("DATASET")
-baker <-
-  read.delim(file = "baker.txt",
-             header = TRUE,
-             row.names = 1)
-# Transforming it to matrix format
-baker <- as.matrix(baker)
-#putting zeros on the diagonal
-diag(baker) <- 0 
-devtools::use_data(baker, overwrite = TRUE)
-devtools::load_all()
