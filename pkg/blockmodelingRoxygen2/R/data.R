6b10c63392bd7068dc174e4975476785a956b357
mihamatj <mihamatj@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2020-06-09 09:50:58 +0000

    Popravek helpfile za baker podatke, kjer sem pri primernih nekaj pozabil dodati in je pri buildu javljalo error
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/blockmodeling@303 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/blockmodelingRoxygen2/R/data.R
+++ pkg/blockmodelingRoxygen2/R/data.R
@@ -2,19 +2,8 @@
 #'
 #' This example consists of the citation data between social 
 #' work journals for the 1985-86 period, collected and analyzed 
-#' in Baker (1992) – see Table 3 below. The dataset covers 20 
-#' journals and the valued directional ties represents the total
-#' number of (discrete) directional citations between and within
-#' journals. Tie values range between 2 and 356, with a mean,
-#' median and standard deviation of, respectively, 33.6, 16,
-#' and 48.9. Apart from Baker’s original analysis, the dataset
-#' has been used as an example to demonstrate core-periphery
-#' heuristics (Borgatti & Everett, 2000), 2-mode generalized
-#' blockmodeling (Doreian, Batagelj, & Ferligoj, 2005, p. 267ff),
-#' and the direct and indirect deviational 
-#' blockmodeling approach (Nordlund, 2016),
-#' latter studies that exclude the diagonal values.
-#'
+#' in Baker (1992) 
+#' 
 #' @docType data
 #'
 #' @usage data(baker)
@@ -27,9 +16,9 @@
 #'
 #'
 #' @examples
-#' data(baker)
-#' Transforming it to matrix format
-#' baker <- as.matrix(baker)
-#' putting zeros on the diagonal
-#' diag(baker) <- 0 
+#' # data(baker)
+#' # Transforming it to matrix format
+#' # baker <- as.matrix(baker)
+#' # putting zeros on the diagonal
+#' # diag(baker) <- 0 
 "baker"
