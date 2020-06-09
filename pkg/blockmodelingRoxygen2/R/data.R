9110319d12107542e6fe34b1854422341403eded
mihamatj <mihamatj@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2020-06-09 07:14:54 +0000

    Helpfile for baker data in work
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/blockmodeling@294 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

---
+++ pkg/blockmodelingRoxygen2/R/data.R
@@ -0,0 +1,24 @@
+#' Prices of 50,000 round cut diamonds.
+#'
+#' This example consists of the citation data between social 
+#' work journals for the 1985-86 period, collected and analyzed 
+#' in Baker (1992) – see Table 3 below. The dataset covers 20 
+#' journals and the valued directional ties represents the total
+#' number of (discrete) directional citations between and within
+#' journals. Tie values range between 2 and 356, with a mean,
+#' median and standard deviation of, respectively, 33.6, 16,
+#' and 48.9. Apart from Baker’s original analysis, the dataset
+#' has been used as an example to demonstrate core-periphery
+#' heuristics (Borgatti & Everett, 2000), 2-mode generalized
+#' blockmodeling (Doreian, Batagelj, & Ferligoj, 2005, p. 267ff),
+#' and the direct and indirect deviational blockmodeling approach (Nordlund, 2016),
+#'  latter studies that exclude the diagonal values.
+#'
+#' @format A data frame with 20 rows and 20 variables:
+#' \describe{
+#'   \item{}{}
+#'   \item{}{}
+#'   ...
+#' }
+#' @source \url{}
+"baker"
